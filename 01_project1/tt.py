97649e5c22bc9de49cebab51c586e3ccef7013a4
chrpe80 <chrpe80@gmail.com>
2024-02-15 16:51:52 +0100

    added some material to help teacher review

--- 01_project1/tt.py
+++ 01_project1/tt.py
@@ -1,7 +1,7 @@
-tt1 = "Given this information: {information}, Create the title"
+tt1 = "Given this information: {information}, create the title. Example title between backticks: ```The Ultimate Python Course```"
 
-tt2 = "Given the title '{title}', create the subtitle"
+tt2 = "Given the title '{title}', create the subtitle. Example subtitle between backticks: ```Follow Me On a Journey Through Python Land```"
 
-tt3 = "Given the title '{title}' and the subtitle '{subtitle}', create the description (maximum 3 lines)"
+tt3 = "Given the title '{title}' and the subtitle '{subtitle}', create the course description (maximum 4 lines)"
 
 tt4 = "Make a recommendation on font(s) and fontsize(s) for {title}, {subtitle} and {description}"
